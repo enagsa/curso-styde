133efc393fae3f055d79e05623c4e102934040d0
enagsa <enagsa@gmail.com>
2018-05-17 19:07:26 +0200

    Lección 17 - Relaciones con Eloquent ORM

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Profession extends Model
 {
     public $timestamps = false;
+    protected $fillable = ['title'];
+
+    public function users(){
+    	return $this->hasMany(User::class);
+    }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -20,7 +20,22 @@ class UserSeeder extends Seeder
         	'name' => 'Enrique Aguilar',
         	'email' => 'enriqueaguilar@expacioweb.com',
         	'password' => bcrypt('1234aA@'),
+            'profession_id' => $profession_id,
+            'is_admin' => true
+        ]);
+
+        User::create([
+            'name' => 'Jesús Guerrero',
+            'email' => 'jesusguerrero@expacioweb.com',
+            'password' => bcrypt('1234aA@'),
             'profession_id' => $profession_id
+        ]);
+
+        User::create([
+            'name' => 'Manu Páez',
+            'email' => 'mauelpaez@expacioweb.com',
+            'password' => bcrypt('1234aA@'),
+            'profession_id' => null
         ]);
 
         /*DB::statement('SET FOREIGN_KEY_CHECKS = 0');
